35c51cceb88e198f42bc112716f76a8d49d340c7
Sébastien Ferry <sebas@10-MBP13-SFERRY.sas-corp.local>
2018-03-07 21:09:46 +0100

    Adding JWT support

--- CodeHike.Microservices.HttpHealthCheck/HttpHealthCheckExtensions.cs
+++ CodeHike.Microservices.HttpHealthCheck/HttpHealthCheckExtensions.cs
@@ -75,7 +75,8 @@ namespace CodeHike.Microservices.HttpHealthCheck
                                 return;
                             }
 
-                            byte[] secretKey=new byte[]{164,60,194,0,161,189,41,38,130,89,141,164,45,170,159,209,69,137,243,216,191,131,47,250,32,107,231,117,37,158,225,234};
+                            byte[] secretKey = System.Text.Encoding.UTF8.GetBytes("magicString");
+                            // new byte[]{164,60,194,0,161,189,41,38,130,89,141,164,45,170,159,209,69,137,243,216,191,131,47,250,32,107,231,117,37,158,225,234};
 
                             string json = Jose.JWT.Decode(token, secretKey);
 
