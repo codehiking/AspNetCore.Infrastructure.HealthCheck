e0923544679c6ddfc63e7acfbf187c2212aacba0
Sébastien F <sferry@smartadserver.com>
2018-03-18 10:52:16 +0100

    Clean-up, logging, config.

--- WebApiExample/Program.cs
+++ WebApiExample/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -13,14 +12,24 @@ namespace WebApiExample
 {
     public class Program
     {
-        public static void Main (string[] args)
+        public static void Main(string[] args)
         {
-            BuildWebHost (args).Run ();
+            IWebHost webHost = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((a, b) => b
+                    .AddJsonFile("appsettings.json", false, true)
+                    .AddJsonFile($"appsettings.{a.HostingEnvironment.EnvironmentName}.json", true, true))
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
+                .Build();
+
+            webHost.Run();
         }
-        
-        public static IWebHost BuildWebHost (string[] args) =>
-            WebHost.CreateDefaultBuilder (args)
-            .UseStartup<Startup> ()
-            .Build ();
     }
 }
